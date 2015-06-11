dd6f3bbeb221b02dd09f5bed1434c48e0ceb73fe
Pierre Rouanet <pierre.rouanet@gmail.com>
2015-06-11 17:39:27 +0200

    Add drive and stop methods.

--- software/poppy_dragster/poppy_dragster.py
+++ software/poppy_dragster/poppy_dragster.py
@@ -5,4 +5,12 @@ from poppy.creatures import AbstractPoppyCreature
 class PoppyDragster(AbstractPoppyCreature):
     @classmethod
     def setup(cls, robot):
-        pass
+        def drive(speed):
+            robot.l_wheel.moving_speed = speed
+            robot.r_wheel.moving_speed = -speed
+
+        def stop():
+            drive(0)
+
+        robot.drive = drive
+        robot.stop = stop
